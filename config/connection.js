4b24c98ed443153a9cc0e9553a16c88a8646acde
msgctcheng <msgctcheng@gmail.com>
2017-10-05 16:38:39 -0400

    Messing with heroku

--- config/connection.js
+++ config/connection.js
@@ -8,15 +8,16 @@ var connection = mysql.createConnection({
     database: "burgers_db",
     PORT: 3306
 });
+
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
+     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
         password: "hacktheplanet",
-        database: "todoagain_db"
-    });
+         database: "todoagain_db"
+     });
 };
 connection.connect( function(err) {
     if (err) {
