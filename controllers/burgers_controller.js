fef4be98b69f149e98c9aa1174f3b3799f06554a
msgctcheng <msgctcheng@gmail.com>
2017-10-05 15:54:38 -0400

    Fixed creating burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,7 +13,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-    burger.insertOne("name", req.body.name, function(result) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.name, false], function(result) {
         res.json({ id: result.insertId });
     });
 });
