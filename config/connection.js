a58dcb65f9b309eeb78636886e7818558f8a253d
msgctcheng <msgctcheng@gmail.com>
2017-10-05 16:33:07 -0400

    add code for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,16 @@ var connection = mysql.createConnection({
     database: "burgers_db",
     PORT: 3306
 });
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "hacktheplanet",
+        database: "todoagain_db"
+    });
+};
 connection.connect( function(err) {
     if (err) {
         console.log("Connection Error: " + err.stack);
